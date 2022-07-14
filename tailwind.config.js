d1b92bf1d0672fdca710074784043f21188823fe
Mário Monteiro <marioammonteiro@gmail.com>
2022-07-14 22:39:23 +0100

    tailwind update to 3.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 const { fontFamily } = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  // or 'media' or 'class'
-  darkMode: false,
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     fontFamily: {
       sans: ['Varela Round', ...fontFamily.sans],
     },
-    extend: {},
-  },
-  variants: {
+
     extend: {},
   },
   plugins: [
